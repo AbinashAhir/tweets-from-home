84cf5e2e8d474ed217cb08ef4115e9d2dceeb7c3
Abinash Ahir <ahirabinash@gmail.com>
2021-07-20 08:15:35 +0530

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HomeIcon from '@material-ui/icons/Home';
+import {ReactComponent as ReactLogo} from './Bread_Frontend_assignment.svg';
+
+function Header() {
+    return (
+      <header>
+      <ReactLogo/>
+      </header>
+
+    );
+  }
+  
+  export default Header;
+  
